21979f48b047b466b6c65509cc0fb2234b8680f3
Sharyl D. Hammer <sharyl1266@gmail.com>
2018-09-23 13:05:58 -0500

    project pig latin

---
+++ Module9Project/frmPigLatin.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Module9Project
+{
+    partial class frmIgPayAtinLay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtconQuote = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnPigLatin = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblEnterText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtconQuote
+            // 
+            this.txtconQuote.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtconQuote.Location = new System.Drawing.Point(120, 45);
+            this.txtconQuote.Name = "txtconQuote";
+            this.txtconQuote.Size = new System.Drawing.Size(848, 34);
+            this.txtconQuote.TabIndex = 0;
+            this.txtconQuote.TextChanged += new System.EventHandler(this.txtconQuote_TextChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(886, 115);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(82, 33);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(674, 115);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 33);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnPigLatin
+            // 
+            this.btnPigLatin.Location = new System.Drawing.Point(477, 115);
+            this.btnPigLatin.Name = "btnPigLatin";
+            this.btnPigLatin.Size = new System.Drawing.Size(75, 33);
+            this.btnPigLatin.TabIndex = 3;
+            this.btnPigLatin.Text = "PigLatin";
+            this.btnPigLatin.UseVisualStyleBackColor = true;
+            this.btnPigLatin.Click += new System.EventHandler(this.btnPigLatin_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblMessage.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(27, 195);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(941, 285);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblMessage.Click += new System.EventHandler(this.lblMessage_Click);
+            // 
+            // lblEnterText
+            // 
+            this.lblEnterText.AutoSize = true;
+            this.lblEnterText.Location = new System.Drawing.Point(12, 62);
+            this.lblEnterText.Name = "lblEnterText";
+            this.lblEnterText.Size = new System.Drawing.Size(89, 17);
+            this.lblEnterText.TabIndex = 5;
+            this.lblEnterText.Text = "Enter Quote:";
+            // 
+            // frmIgPayAtinLay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(1000, 502);
+            this.Controls.Add(this.lblEnterText);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnPigLatin);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtconQuote);
+            this.Name = "frmIgPayAtinLay";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IgPay AtinLay";
+            this.Load += new System.EventHandler(this.frmIgPayAtinLay_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtconQuote;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnPigLatin;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblEnterText;
+    }
+}
+
---
+++ Module9Project/frmPigLatin.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*Sharyl Hammer
+ * ITD 1253 C#
+ * Fall 2016
+ * November 30, 2016
+ */
+namespace Module9Project
+{
+    public partial class frmIgPayAtinLay : Form
+    {
+        public frmIgPayAtinLay()
+        {
+            InitializeComponent();
+        }
+        public bool IsValidData()
+        {
+            return
+                IsPresent(txtconQuote, "Translate Sentence");
+
+        }
+
+        public bool IsPresent(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                lblMessage.Text = "The textbox can not be blank. Please enter a quote.";
+                textBox.Focus();
+                return false; 
+            }
+
+            return true;
+        }
+
+
+
+
+        private string GetPigLatin(string RawText)
+          {
+          //  string word = "";
+            string pigword = "";
+            string firstLetter;
+            string restofWord;
+            string vowels = "AEIOUaeiou";
+            int letterPos;
+            string hold_value = " ";
+
+            int wordPos;
+            
+            string con_word = "";
+            string rclean = "";
+
+            RawText = RawText.Trim();
+            rclean = RawText.Replace(".", "");
+            RawText = rclean;
+
+            
+           // string[] words = RawText.Split(' ');
+           // string words = RawText.Split(' ');
+            // We have split the string into words!!
+            // Now we have to manipulate each word.
+            
+
+
+
+            foreach(string word in RawText.Split())
+            {
+                firstLetter = word.Substring(0, 1);
+                restofWord = word.Substring(1, word.Length - 1);
+
+                letterPos = vowels.IndexOf(firstLetter);
+               
+
+                if (letterPos == -1)
+                {
+                    // we have found a constant
+                    // we have to determine if there is a vowel in the word.
+                    // place restofWord in loop.
+
+                    // string[] letter = restofWord.Split(' ');  
+
+                    for (int i = 0; i < restofWord.Length; i++)
+
+                    {
+
+                      //  letterPos = vowels.IndexOf(firstLetter);
+                        hold_value = restofWord[i].ToString();
+                        wordPos = vowels.IndexOf(hold_value);
+                        if (wordPos == -1)
+                        {
+                            if (con_word == "")
+                            {
+                                con_word = restofWord;
+                                con_word = con_word + firstLetter + restofWord[i];
+                                con_word = con_word.Remove(0, 1);
+                            }
+                            else
+                            {
+                                con_word = con_word + restofWord[i];
+                                con_word = con_word.Remove(0, 1);
+                            }
+
+
+                        }
+                        else
+                        {
+                            //need to break out of loop
+                            break;
+                        }
+                        
+                   }
+                    if (con_word == "")
+                    {
+                        pigword += restofWord + firstLetter + "ay ";
+                    }
+                    else
+                    {
+                        
+                        pigword += con_word + "ay ";
+                    }
+
+                    con_word = "";
+                }
+                      
+                else
+                   // we have found a vowel
+                {
+                pigword += word + "way ";
+
+                
+                }
+
+
+        }
+            return pigword;
+     }
+
+           
+
+     
+        private void frmIgPayAtinLay_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+
+        {
+            lblMessage.Text = "";
+            txtconQuote.Text = "";
+            txtconQuote.Focus();
+        }
+
+        private void btnPigLatin_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                string RawText;
+
+                string pigquote = "";
+
+                RawText = txtconQuote.Text;
+              
+                // Validate here.
+                if (IsValidData())
+                {
+                    pigquote = this.GetPigLatin(RawText);
+                    //Displays Pig Latin Quote.
+                    int a = pigquote.Length;
+             
+                    // Removing extra space and placing period at end of pigquote.
+                    pigquote = pigquote.Remove(pigquote.Length - 1);
+                    pigquote = pigquote + ".";
+                    lblMessage.Text = pigquote.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message + "\n\n";
+            }
+           
+
+        }
+
+        private void txtconQuote_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMessage_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
